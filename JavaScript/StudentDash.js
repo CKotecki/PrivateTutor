30fb8d463c5f2bf83bd7aab4b5fe0fdb69eabcdd
gradyShepherd <gradyshepherd13@gmail.com>
2017-02-12 22:04:14 -0700

    created folders organized files updated html pages to current

--- JavaScript/StudentDash.js
+++ JavaScript/StudentDash.js
@@ -85,10 +85,11 @@ function calendarBuilder(month, year)
 		//};
 		
 		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() {
+		/*
+		span.onclick(function() {
 			modal.style.display = "none";
-		};
-		
+		});
+		*/
 		// When the user clicks anywhere outside of the modal, close it
 		window.onclick = function(event) {
 			if (event.target == modal) {
