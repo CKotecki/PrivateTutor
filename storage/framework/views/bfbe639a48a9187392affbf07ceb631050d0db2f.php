1a844e3045737d44b1ddbe5abaa8c54b11452b68
ljaynes <ljaynes87@gmail.com>
2017-03-01 12:54:11 -0700

    hopefully added all the files this time

---
+++ storage/framework/views/bfbe639a48a9187392affbf07ceb631050d0db2f.php
@@ -0,0 +1,47 @@
+<?php $__env->startSection('content'); ?>
+
+<div class="wrapper">
+    <div class="Left_Partial" id="studentDiv">
+        <img src="img/students.png" id="student-img" class="Images">
+         <h2>STUDENTS</h2>
+
+        <p class="student-div-content">
+           Improve your math grade and get through homework faster. Our expert tutors are available to help you 24/7. Whether you need help at 11PM with Algebra I or want to recap geometry concepts before class, our tutors are here to help. From statistics and pre-algebra to geometry and calculus, we cover it all.
+
+Stuck on Algebraic expressions the night before a test? Log on and connect to a tutor. It's that easy. Online access means you can get help the moment you need it. Grasp the basics of algebra or review geometry proofs. No matter the topic or the time, we have a tutor that is ready to help.
+        </p>
+
+ <h2>Why you should use Lesson Logistics</h2>
+        <ul class="student-div-content">
+            <li>Find tutors quickly and efficiently</li>
+            <li>Find tutors who can meet your learning needs</li>
+            <li>Schedule sessions</li>
+            <li>Pay online</li>
+            <li>Receive help 24/7</li>
+        </ul>
+    </div>
+
+
+
+    <div class="Right_Partial" id="tutorDiv">
+        <img src="img/teacher.png" id="tutor-img" class="Images">
+
+        <h2 style="color: #F4F6F5">TUTORS</h2>
+
+        <p class="tutor-div-content">
+            To be an online tutor you need to have a good grasp of the concepts in the subject you want to tutor in. Having the ability to relate topics to real-world examples and explain them to the students helps students understand how they can apply what they are learning.
+  Good communication skills and lots of patience are essential too. Sometimes it is very difficult to tutor someone online who might not have the basic foundation for the particular subject. As the tutor, you have to detect, adapt, and teach to the student so that they can fully understand the concept.
+        </p>
+
+<h2 style="color: #F4F6F5">Why you should use Lesson Logistics</h2>
+        <ul class="student-div-content">
+            <li>Build the reputation you deserve</li>
+            <li>Make your presence known, and take your business online and broaden your prospects</li>
+            <li>Schedule sessions</li>
+            <li>Manage your payments online</li>
+            <li>Provide online materials and resources</li>
+        </ul>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
